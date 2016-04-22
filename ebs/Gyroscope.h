7d0d4bd811e46fbbcafac1aac21ff9e96a027053
Peter Kowalczyk <peter@slix.io>
2016-04-21 22:09:52 -0500

    Make class constants static

--- ebs/Gyroscope.h
+++ ebs/Gyroscope.h
@@ -43,25 +43,25 @@ class Gyroscope
     int raw_read_z_axis();
 
     // Register list
-    const char WHO_AM_I = 0x00;
-    const char SMPLRT_DIV= 0x15;
-    const char DLPF_FS = 0x16;
-    const char GYRO_XOUT_H = 0x1D;
-    const char GYRO_XOUT_L = 0x1E;
-    const char GYRO_YOUT_H = 0x1F;
-    const char GYRO_YOUT_L = 0x20;
-    const char GYRO_ZOUT_H = 0x21;
-    const char GYRO_ZOUT_L = 0x22;
+    static const char WHO_AM_I = 0x00;
+    static const char SMPLRT_DIV= 0x15;
+    static const char DLPF_FS = 0x16;
+    static const char GYRO_XOUT_H = 0x1D;
+    static const char GYRO_XOUT_L = 0x1E;
+    static const char GYRO_YOUT_H = 0x1F;
+    static const char GYRO_YOUT_L = 0x20;
+    static const char GYRO_ZOUT_H = 0x21;
+    static const char GYRO_ZOUT_L = 0x22;
 
     // Setting list to be loaded into registers
-    const char DLPF_CFG_0 = (1<<0);
-    const char DLPF_CFG_1 = (1<<1);
-    const char DLPF_CFG_2 = (1<<2);
-    const char DLPF_FS_SEL_0 = (1<<3);
-    const char DLPF_FS_SEL_1 = (1<<4);
+    static const char DLPF_CFG_0 = (1<<0);
+    static const char DLPF_CFG_1 = (1<<1);
+    static const char DLPF_CFG_2 = (1<<2);
+    static const char DLPF_FS_SEL_0 = (1<<3);
+    static const char DLPF_FS_SEL_1 = (1<<4);
 
     // IC2 address
-    const char ITG_ADDR = 0x69;
+    static const char ITG_ADDR = 0x69;
 
     void itg_write(char i2c_addr, char reg_addr, char data);
     unsigned char itg_read(char i2c_addr, char reg_addr);
