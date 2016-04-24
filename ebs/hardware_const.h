f077d052fcd6570ad76a06e5959c6e4d0432c3bc
Peter Kowalczyk <peter@slix.io>
2016-04-24 02:44:13 -0500

    Add additional hardware constants from @mattpotok 's information

--- ebs/hardware_const.h
+++ ebs/hardware_const.h
@@ -5,9 +5,13 @@ namespace HardwareConst {
   const int SERVO_ATTACH_PIN = 9;
   const int SERVO_FEEDBACK_PIN = A0;
 
-  const int BATTERY_LED_PIN = -1;
-  const int MODE_LED_PIN = -1;
+  const int BATTERY_LED_PIN = 7;
+  const int MODE_LED_PIN = 9;
   const int CALIBRATION_LED_PIN = -1;
+
+  const int DOWN_BUTTON_PIN = 10;
+  const int UP_BUTTON_PIN = 8;
+  const int CALIBRATION_BUTTON_PIN = 1;
 }
 
 #endif //EBS_HARDWARE_CONST_H
